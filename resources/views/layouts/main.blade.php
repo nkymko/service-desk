5fec2b3905c6e6771e7f0df24aeab8f3bfd948a1
Anfebriy17 <19200483@bsi.ac.id>
2023-08-23 08:51:36 +0700

    commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,21 +15,26 @@
     {{-- google fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
     {{-- custom --}}
-    <link rel="stylesheet" href="{{ asset('css/'. $style .'.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/' . $style . '.css') }}">
 
     <title>{{ $title }}</title>
 </head>
-<body style="min-height: 2000px">
 
-    @yield('base')
 
-    {{-- script --}}
-    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+@yield('base')
 
-    @yield('script')
+{{-- script --}}
+<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+</script>
+
+@yield('script')
 </body>
+
 </html>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,47 +1,44 @@
 @extends('layouts.main')
 
 @section('base')
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 
-<body>
-    <div class="wrapper">
-        <div class="container main">
-            <div class="row">
-                <div class="col-md-6 side-image">
-                    <!-----image-------->
-                    <img src="{{ asset('img/lambang.png') }}" alt="">
-                    <div class="text">
-                        <p>Selamat Datang kembali di Website Service Desk<i>-Kominfotik</i></p>
+    <body>
+        <div class="wrapper">
+            <div class="container main">
+                <div class="row">
+                    <div class="col-md-6 side-image">
+                        <!-----image-------->
+                        <img src="{{ asset('img/lambang.png') }}" alt="">
+                        <div class="text">
+                            <p>Selamat Datang kembali di Website Service Desk<i>-Kominfotik</i></p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6 right">
-                    <div class="input-box">
-                        <header>Create account</header>
-                        <div class="input-field">
-                            <input type="text" class="input" id="name" required autocomplete="off">
-                            <label for="name">Nama</label>
-                        </div>
-                        <div class="input-field">
-                            <input type="text" class="input" id="email" required autocomplete="off">
-                            <label for="email">Email</label>
-                        </div>
-                        <div class="input-field">
-                            <input type="password" class="input" id="password" required>
-                            <label for="password">Password</label>
-                        </div>
-                        <div class="input-field">
-                            <input type="submit" class="submit" value="Sign Up">
-                        </div>
-                        <div class="signin">
-                            <span>Already have an account? <a href="#">Log in here</a></span>
+                    <div class="col-md-6 right">
+                        <div class="input-box">
+                            <header>Create account</header>
+                            <div class="input-field">
+                                <input type="text" class="input" id="name" required autocomplete="off">
+                                <label for="name">Nama</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="text" class="input" id="email" required autocomplete="off">
+                                <label for="email">Email</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="password" class="input" id="password" required>
+                                <label for="password">Password</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="submit" class="submit" value="Sign Up">
+                            </div>
+                            <div class="signin">
+                                <span>Already have an account? <a href="#">Log in here</a></span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
-
-
-
-
+    </body>
 @endsection
